9c8d88c5381bfb5336c2b6099d5c3ed328b51084
Shima <h.sima710@gmail.com>
2021-04-04 15:36:13 +0900

    v1.0.1
    ・整頓
    ・DGPlayer追加

--- src/main/java/net/shmn7iii/disgot/Disgot.java
+++ src/main/java/net/shmn7iii/disgot/Disgot.java
@@ -1,7 +1,5 @@
 package net.shmn7iii.disgot;
 
-import net.shmn7iii.disgot.discord.discmain;
-import net.shmn7iii.disgot.spigot.Event;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
@@ -13,7 +11,7 @@ public final class Disgot extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-        getServer().getPluginManager().registerEvents(new Event(this), this);
+        getServer().getPluginManager().registerEvents(new SpigotEvent(this), this);
         saveDefaultConfig();
         Config.load();
         getLogger().info("Hello!");
